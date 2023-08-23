54a51e681c6e9b8ae0fa7aeb9fd3548f1fe3bfe9
ZCR <zcr20080901@163.com>
2023-08-23 09:43:39 +0800

    编写scrapy.Item类和Station类和相关子类

--- publicTriffic/publicTriffic/items.py
+++ publicTriffic/publicTriffic/items.py
@@ -9,8 +9,44 @@ import scrapy
 class LineItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    province = scrapy.Field()
     city = scrapy.Field()
-    busType = scrapy.Field()
-    time = scrapy.Field()  # 运营时间
+
     stationList = scrapy.Field()  # 站点名录
 
+class BusItem(LineItem):
+    busType = scrapy.Field()  #公交类型
+    time = scrapy.Field()  # 运营时间
+
+
+class SubwayItem(LineItem):
+    color = scrapy.Field() # 代表色
+    zhixian = scrapy.Field() #是否有支线
+    zhixianList = scrapy.Field() #支线表
+
+
+class Station:
+    def __init__(self, name,):
+        self.name = name
+
+class SubwayStation(Station):
+    def __init__(self, name,time,fencha,huancheng,line,exitList):
+        super(Station, self).__init__(name)
+        self.time = time
+        self.fencha =fencha
+        self.huancheng =huancheng
+        self.line = line
+        self.exitList = exitList
+
+
+
+class BusStation(Station):
+    def __init__(self, name,line,subway):
+        super(BusStation, self).__init__(name)
+        self.line = line
+        self.subway = subway  # 地铁换乘信息
+
+class TrafficWeb(scrapy.Item):
+    stationList = scrapy.Field()
+    lineList = scrapy.Field()
+    city = scrapy.Field()
